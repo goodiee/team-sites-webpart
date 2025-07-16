bfcefad8028988eccdf4b0b0ac14a69789c2f744
Maksym <bondextv@gmail.com>
2025-07-16 14:08:57 +0300

    Initial commit of SPFx project

---
+++ src/webparts/myTeamSites/components/IMyTeamSitesProps.ts
@@ -0,0 +1,11 @@
+export interface IMyTeamSitesProps {
+  description: string;
+  test: string;
+  test2: string;
+  test1: boolean;
+  test3: boolean;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
